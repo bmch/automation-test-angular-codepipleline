96a4672df86959546217272f6b208d905ddc3a27
Brendan McHugh <brendanmchugh12@gmail.com>
2020-12-09 23:58:14 +0000

    amend css grid, add score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,11 @@ import * as data from './trivia.json';
 })
 export class AppComponent implements OnInit {
   questions: any = (data as any).default;
-  current: number = 0;
+  current: number = 9;
 
-  // store both on new memory object
-  answerCorrect: boolean;
-  answerPreselected: string = "";
-  answerSubmitted: boolean = false;
   score = 0
-
+  incorrect = 0;
+  displayFinalScore = false;
 
   ngOnInit() {
     // load questions from api if necessary
@@ -32,10 +29,18 @@ export class AppComponent implements OnInit {
     }
     if (this.questions[this.current].answerPreselected !== this.questions[this.current].answer) {
       this.questions[this.current].answerCorrect = false;
+      this.incorrect++;
     } else {
+      this.score++
       this.questions[this.current].answerCorrect = true;
     }
     this.questions[this.current].answerSubmitted = true;
+    if (this.current == this.questions.length - 1) {
+      //display final score 
+      this.displayFinalScore = true;
+      console.log('finished display score')
+    }
+
   }
 
   goToNextQuestion() {
